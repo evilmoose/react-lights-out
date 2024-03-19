a49e644f9158c41e8e3f0d2a3ee1fc8f3ee1aa90
Evilmoose <ronnielopez@gmail.com>
2024-03-19 11:00:39 -0700

    Board.js Updated

--- src/Cell.js
+++ src/Cell.js
@@ -1,20 +1,7 @@
 import React from "react";
 import "./Cell.css";
 
-/** A single cell on the board.
- *
- * This has no state --- just two props:
- *
- * - flipCellsAroundMe: a function rec'd from the board which flips this
- *      cell and the cells around of it
- *
- * - isLit: boolean, is this cell lit?
- *
- * This handles clicks --- by calling flipCellsAroundMe
- *
- **/
-
-function Cell({ flipCellsAroundMe, isLit }) {
+const Cell = ({ flipCellsAroundMe, isLit }) => {
   const classes = `Cell ${isLit ? "Cell-lit" : ""}`;
   return <td className={classes} onClick={flipCellsAroundMe} />;
 }
